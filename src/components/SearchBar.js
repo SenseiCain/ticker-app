c08a6d49d11c2564ec61259db23031c7082dc25f
Christian Cain <christian22cain@yahoo.com>
2020-09-30 09:54:06 -0500

    created controlled form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 
 const SearchBar = () => {
+    const [query, setQuery] = useState('');
+
+    const handleChange = ({ target: {value} }) => {
+        setQuery(value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setQuery('');
+    }
+
     return ( 
-        <div></div>
+        <div id="searchbar">
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={query} onChange={handleChange} />
+                <input type="submit" />
+            </form>
+        </div>
     )
 }
 
