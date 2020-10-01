f06a5afe6af0a77f0b2e81698e64922ea98a9b4d
Christian Cain <christian22cain@yahoo.com>
2020-10-01 07:20:13 -0500

    removed comments

--- src/context/query-context.js
+++ src/context/query-context.js
@@ -5,10 +5,6 @@ const QueryContext = React.createContext(['', () => {}]);
 const QueryProvider = props => {
     const [state, setState] = useState('');
 
-    // <MusicPlayerContext.Provider value={[state, setState]}>
-    //   {props.children}
-    // </MusicPlayerContext.Provider>
-
     return (
         <QueryContext.Provider value={[state, setState]}>
             {props.children}
