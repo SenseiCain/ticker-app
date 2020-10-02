d8dc57e3fce84ce636febf9e0dd1d2992f49c565
Christian Cain <christian22cain@yahoo.com>
2020-10-02 07:13:25 -0500

    added TickerList to Home

---
+++ src/components/TickerList.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+const TickerList = props => {
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        // console.log('mounted')
+    })
+
+    return(
+        <div></div>
+    )
+}
+
+export default TickerList;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 
 import Logo from '../components/Logo';
 import SearchBar from '../components/SearchBar';
+import TickerList from '../components/TickerList';
 
 const Home = () => {
     return (
         <div>
             <Logo />
             <SearchBar />
+            <TickerList />
         </div>
     )
 }
