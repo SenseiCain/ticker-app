2075f944c82238fb8b8193db1ec083caf0b92508
Christian Cain <christian22cain@yahoo.com>
2020-10-03 07:26:07 -0500

    tested RapidAPI fetch request

--- src/components/TickerList.js
+++ src/components/TickerList.js
@@ -2,10 +2,45 @@ import React, { useState, useEffect } from 'react';
 
 const TickerList = props => {
     const [list, setList] = useState(['AAPL', 'MSFT', 'AMZN']);
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+
+    async function getCompanyData() {
+        const url = 'https://alpha-apis-finance-graphql.p.rapidapi.com/';
+
+        const mutation = `
+            {
+                apple: company(identifier: "AAPL"){
+                    name
+                }
+                
+                microsoft: company(identifier: "MSFT"){
+                    name
+                }
+                
+                amazon: company(identifier: "AMZN"){
+                    name
+                }
+            }`;
+
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "x-rapidapi-host": "alpha-apis-finance-graphql.p.rapidapi.com",
+                "x-rapidapi-key": "c2e6cd2352mshc152fd4126f9197p1e9892jsne99352339c02",
+                "content-type": "application/json",
+                "accept": "application/json",
+                "useQueryString": true
+            },
+            body: JSON.stringify({ query: mutation })
+        });
+        const json = await resp.json();
+
+        console.log(json);
+    }
 
     useEffect(() => {
-        // console.log('mounted')
+        // console.log('mounted');
+        getCompanyData();
     })
 
     return(
