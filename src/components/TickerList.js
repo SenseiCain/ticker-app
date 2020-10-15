a6796cb1dcfb9dfb36436bf3c4ff96dd6c5c7c96
Christian Cain <christian22cain@yahoo.com>
2020-10-15 12:46:49 -0500

    ticker symbol links to CompanyPage

--- src/components/TickerList.js
+++ src/components/TickerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TickerList = ({ title, tickers }) => {
 
@@ -8,7 +9,9 @@ const TickerList = ({ title, tickers }) => {
                 <div key={i}>
                     <ul>
                         <li>{el.name}</li>
-                        <li>{el.ticker}</li>
+                        <li>
+                            <Link to={`/${el.ticker}`}>{el.ticker}</Link>
+                        </li>
                         <li>{el.price}</li>
                         <li>{el.delta}</li>
                     </ul>
