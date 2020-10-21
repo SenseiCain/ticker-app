51675947aa2a22680f703251b9f0fd1c77c04fc5
Christian Cain <christian22cain@yahoo.com>
2020-10-21 09:28:14 -0500

    sketched out chart

--- src/components/CompanyChart.js
+++ src/components/CompanyChart.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
 const CompanyChart = ({ handleChange, chartData }) => {
-    
+    const labels = chartData.map(el => new Date(el.time));
+    console.log(labels)
+    const prices = chartData.map(el => parseFloat(el.price.toFixed(2)));
+
+    const data = {
+        labels,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            data: prices
+        }]
+    }
+
+    const options = {
+        maintainAspectRatio: false,
+        responsive: false,
+        legend: { display: false },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                },
+                ticks: {
+                    // Reduces number of ticks & formats date
+                    callback: function(value, index, values) {
+                        if (index % 3 !== 0) {
+                            return null;
+                        } else {
+                            // TODO - Dif labeling depending on range
+                            return `${value.getHours()}:${value.getMinutes()}`;
+                        }
+                    }
+                }
+            }],
+            yAxes: [{
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                }
+            }]
+        }
+    }
+
     return (
         <div>
             <h1>Chart</h1>
+            <Line data={data} options={options} width={300} height={300} />
             <button onClick={() => handleChange('day')}>Day</button>
             <button onClick={() => handleChange('week')}>Week</button>
             <button onClick={() => handleChange('month')}>Month</button>
