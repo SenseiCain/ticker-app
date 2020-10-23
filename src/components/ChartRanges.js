6087924b8d177602219355521b4cb18eb0ba9fa9
Christian Cain <christian22cain@yahoo.com>
2020-10-23 12:29:39 -0500

    minor sytax correction

--- src/components/ChartRanges.js
+++ src/components/ChartRanges.js
@@ -3,11 +3,11 @@ import React from 'react';
 const ChartRanges = ({ handleChange }) => {
     return (
         <div className="d-flex justify-content-around">
-            <button type="button" class="btn btn-outline-primary btn-sm" onClick={() => handleChange('day')}>Day</button>
-            <button type="button" class="btn btn-outline-primary btn-sm" onClick={() => handleChange('week')}>Week</button>
-            <button type="button" class="btn btn-outline-primary btn-sm" onClick={() => handleChange('month')}>Month</button>
-            <button type="button" class="btn btn-outline-primary btn-sm" onClick={() => handleChange('3month')}>3 Month</button>
-            <button type="button" class="btn btn-outline-primary btn-sm" onClick={() => handleChange('1yr')}>Year</button>
+            <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => handleChange('day')}>Day</button>
+            <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => handleChange('week')}>Week</button>
+            <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => handleChange('month')}>Month</button>
+            <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => handleChange('3month')}>3 Month</button>
+            <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => handleChange('1yr')}>Year</button>
         </div>
     )
 }
