5c27dadbdefab72cba2a91bdf3d2179c7238169b
Christian Cain <christian22cain@yahoo.com>
2020-10-26 14:07:01 -0500

    added error handling

--- src/hooks/useFetchChartData.js
+++ src/hooks/useFetchChartData.js
@@ -3,14 +3,11 @@ import { useState, useEffect } from 'react';
 const useFetchChartData = (ticker, time) => {
     const [chartData, setChartData] = useState([]);
     const [statusChart, setStatusChart] = useState(false);
+        
+    let range;
+    let interval;
 
-    const fetchData = async () => {
-        setChartData([])
-        setStatusChart(true);
-
-        let range;
-        let interval;
-
+    const updateIntervalRange = time => {
         switch(time) {
             case 'day':
                 range = "1d";
@@ -35,8 +32,12 @@ const useFetchChartData = (ticker, time) => {
             default:
                 break;
         }
+    }
 
-        
+    useEffect(() => {
+        setChartData([]);
+        setStatusChart(true);
+        updateIntervalRange(time);
 
         const url = `https://rapidapi.p.rapidapi.com/stock/v2/get-chart?interval=${interval}&symbol=${ticker}&range=${range}&region=US`
 
@@ -45,21 +46,26 @@ const useFetchChartData = (ticker, time) => {
             "x-rapidapi-key": "c2e6cd2352mshc152fd4126f9197p1e9892jsne99352339c02"
         }
 
-        const resp = await fetch(url, { headers });
-        const json = await resp.json();
+        fetch(url, { headers }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+              throw new Error("Couldn't find Company!");
+            }
+          })
+          .then(json => {
+            const result = json.chart.result[0];
+            const returnArr = result.timestamp.map((time, i) => {
+                // Time & Close
+                return { time: new Date(time * 1000), price: result.indicators.quote[0].close[i] }
+            });
 
-        const result = json.chart.result[0];
-        const returnArr = result.timestamp.map((time, i) => {
-            // Time & Close
-            return { time: new Date(time * 1000), price: result.indicators.quote[0].close[i] }
-        });
-
-        setStatusChart(false);
-        setChartData(returnArr);
-    }
-
-    useEffect(() => {
-        fetchData();
+            setStatusChart(false);
+            setChartData(returnArr);
+          })
+          .catch((error) => {
+            setStatusChart(false);
+          });
     }, [ticker, time])
 
     return { chartData, statusChart }
