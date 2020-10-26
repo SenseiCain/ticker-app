4be9a730fefa243232b678d890372f3d15e0b66f
Christian Cain <christian22cain@yahoo.com>
2020-10-26 12:07:50 -0500

    displays placeholder on loading

--- src/components/CompanyInfo.js
+++ src/components/CompanyInfo.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CompanyInfo= ({ info }) => {
+    const [state, setState] = useState({
+         hq: '',
+         indsutry: '',
+         ceo: '',
+         fte: '',
+         website: '',
+         description: ''
+         });
 
-    const genInfo = data => {
-        if(data) {
-
-            const { hq, ceo, industry, website, description, fte } = data;
-
-            return (
-                <div className="pt-4">
-                    <ul className="list-unstyled">
-                        <li className="pb-2">
-                            HQ: <span className="text-secondary">{hq}</span>
-                        </li>
-                        <li className="pb-2">
-                            Industry: <span className="text-secondary">{industry}</span>
-                        </li>
-                        <li className="pb-2">
-                            CEO: <span className="text-secondary">{ceo}</span>
-                        </li>
-                        <li className="pb-2">
-                            FTE: <span className="text-secondary">{fte}</span>
-                        </li>
-                        <li className="pb-2">
-                            Website: <a href={website} target="_blank">{website}</a>
-                        </li>
-                        <li className="crop-text">
-                            Description: <span className="text-secondary">{description}</span>
-                        </li>
-                        
-                    </ul>
-                </div>
-            )
-        } else {
-            return 'loading...'
+    useEffect(() => {
+        if(info){
+            setState({ 
+                hq: info.hq,
+                industry: info.industry,
+                ceo: info.ceo,
+                fte: info.fte,
+                website: info.website,
+                description: info.description
+            })
         }
-    }
+    }, [info])
+
 
     return (
         <div>
-            {genInfo(info)}
+            <div className="pt-4">
+                    <ul className="list-unstyled">
+                        <li className="pb-2">
+                            HQ: <span className="text-secondary">{state.hq}</span>
+                        </li>
+                        <li className="pb-2">
+                            Industry: <span className="text-secondary">{state.industry}</span>
+                        </li>
+                        <li className="pb-2">
+                            CEO: <span className="text-secondary">{state.ceo}</span>
+                        </li>
+                        <li className="pb-2">
+                            FTE: <span className="text-secondary">{state.fte}</span>
+                        </li>
+                        <li className="pb-2">
+                            Website: <a href={state.website} target="_blank">{state.website}</a>
+                        </li>
+                        <li className="crop-text">
+                            Description: <span className="text-secondary">{state.description}</span>
+                        </li>
+                    </ul>
+                </div>
         </div>
     )
 }
